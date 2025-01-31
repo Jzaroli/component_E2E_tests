592c5fa6f2009dda808e4b146ffcd314bfb9be08
Jzaroli <your-GitHub-username@users.noreply.github.com>
2025-01-31 12:52:10 -0700

    added Cypress files the way they were structured in previous repo

---
+++ cypress/e2e/quiz.cy.js
@@ -0,0 +1,34 @@
+describe('Quiz Component', () => {
+    beforeEach(() => {
+        cy.visit('/');
+    });
+
+    it('starts the quiz and displays the first question', () => {
+        cy.get('buton').contains('Start Quiz').click();
+        cy.get('card').should('be.visible');
+        cy.get('h2').should('not.be.empty');
+    });
+
+    it('answers questions and completes the quiz', () => {
+        cy.get('button').contains('Start Quiz').click();
+
+        for (let i = 0; i < 10; i++) {
+        cy.get('button').contains('1').click();
+        }
+
+        cy.get('.alert-success').should('be.visible').and('contain', 'Your score');
+    });
+
+    it('restarts the quiz after completion', () => {
+        cy.get('button').contains('Start Quiz').click();
+        for (let i = 0; i <10; i++) {
+        cy.get('button').contains('1').click();
+        }
+        
+        cy.get('button').contains('Take New Quiz').click();
+
+        cy.get('.card').should('be.visible');
+        cy.get('h2').should('not.be.empty');
+    });
+
+});
